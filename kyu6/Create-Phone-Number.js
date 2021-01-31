fba46525d5de7ba2f5836e954d9687ebee954aad
flavian mohanraj <flavianmohanraj@gmail.com>
2021-01-31 11:45:22 -0500

    included question and tests

--- kyu6/Create-Phone-Number.js
+++ kyu6/Create-Phone-Number.js
@@ -6,3 +6,9 @@ function createPhoneNumber(numbers){
   }
   return format
 }
+
+describe('Basic tests', () => {
+  Test.assertEquals(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+  Test.assertEquals(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+  Test.assertEquals(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+});
