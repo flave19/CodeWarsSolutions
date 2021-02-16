d58506870dee32d891e09a91fd6c56e337965e3d
flavian mohanraj <flavianmohanraj@gmail.com>
2021-02-16 08:21:32 -0500

    Credit Card Mask

--- 7 kyu/Credit-Card-Mask.js
+++ 7 kyu/Credit-Card-Mask.js
@@ -2,13 +2,14 @@
 
 function maskify(cc) {
   let newstr = "";
-  if (cc.length < 5) {
+  if (cc.length < 4) {
     return cc;
   } else {
     for (let i = 0; i < cc.length - 4; i++) {
       newstr += "#";
     }
     let fourDigits = cc.slice(-4);
-    return newstr.concat(arr);
+    return newstr.concat(fourDigits);
   }
 }
+
