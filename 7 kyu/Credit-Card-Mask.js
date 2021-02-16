853be06ec208eab68ea0176077bbd304b64e0a51
flavian mohanraj <flavianmohanraj@gmail.com>
2021-02-16 08:14:27 -0500

    Credit Card Mask

---
+++ 7 kyu/Credit-Card-Mask.js
@@ -0,0 +1,14 @@
+//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+function maskify(cc) {
+  let newstr = "";
+  if (cc.length < 5) {
+    return cc;
+  } else {
+    for (let i = 0; i < cc.length - 4; i++) {
+      newstr += "#";
+    }
+    let fourDigits = cc.slice(-4);
+    return newstr.concat(arr);
+  }
+}
