96e5d8b4f0dc5512acb4370ab3258d4329ec5e8c
flavian mohanraj <flavianmohanraj@gmail.com>
2021-03-01 23:16:57 -0500

    Detect Pangram

--- 6 kyu/Detect-Pangram.js
+++ 6 kyu/Detect-Pangram.js
@@ -1,9 +1,7 @@
 //Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
 function isPangram(string){
-  let alphabet = "abcdefghijklmnopqrstuvwxyz"
-  let letters = alphabet.split("")
-  let str = string.split("")
-  let filtered = letters.filter((word) => !str.includes(word))
-  return filtered.length === 0 ? true : false
+    return 'abcdefghijklmnopqrstuvwxyz'
+      .split('')
+      .every((x) => string.toLowerCase().includes(x));
 }
